e51277ca56b3d2c39a3e1980bb07c6075cdbd53c
ole <gitlab+account@ole.mn>
2022-12-13 18:30:31 +0100

    remove unnecessary overlay

--- flake.nix
+++ flake.nix
@@ -4,25 +4,22 @@
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
   outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-        my-name = "nps";
-        my-buildInputs = with pkgs; [ getopt ripgrep ];
-        nps = (pkgs.writeScriptBin my-name (builtins.readFile ./nps)).overrideAttrs(old: {
-          buildCommand = "${old.buildCommand}\n patchShebangs $out";
-        });
-      in rec {
-        defaultPackage = packages.nps;
-        packages.nps = pkgs.symlinkJoin {
-          name = my-name;
-          paths = [ nps ] ++ my-buildInputs;
-          buildInputs = [ pkgs.makeWrapper ];
-          postBuild = "wrapProgram $out/bin/${my-name} --prefix PATH : $out/bin";
-        };
-	overlay = {
-	  nps = nps.defaultPackage.${system};
-	};
+  flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs { inherit system; };
+      my-name = "nps";
+      my-buildInputs = with pkgs; [ getopt ripgrep ];
+      nps = (pkgs.writeScriptBin my-name (builtins.readFile ./nps)).overrideAttrs(old: {
+        buildCommand = "${old.buildCommand}\n patchShebangs $out";
+      });
+    in rec {
+      defaultPackage = packages.nps;
+      packages.nps = pkgs.symlinkJoin {
+        name = my-name;
+        paths = [ nps ] ++ my-buildInputs;
+        buildInputs = [ pkgs.makeWrapper ];
+        postBuild = "wrapProgram $out/bin/${my-name} --prefix PATH : $out/bin";
       };
-    );
+    }
+  );
 }
